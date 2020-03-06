6c15bcc20621651b9a955209b9f7364ebd0dd7f0
Nida Jamshed <njamshed92@gmail.com>
2020-03-05 22:42:01 -0500

    Sharing Project with Team members.

---
+++ app/src/main/java/com/sdetproject2020/gringottsbanks/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package com.sdetproject2020.gringottsbanks.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import com.sdetproject2020.gringottsbanks.data.LoginDataSource;
+import com.sdetproject2020.gringottsbanks.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
